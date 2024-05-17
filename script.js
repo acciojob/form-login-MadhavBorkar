b7db318b87a7eb2d9d437c7ae34c4c3882644db3
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-17 23:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the values of the First Name and Last Name fields
+    const FName = document.getElementById("first-name").value;
+    const LName = document.getElementById("last-name").value;
+
+    // Display an alert with the first and last name
+    alert(`${firstName} ${lastName}`);
 
 }
